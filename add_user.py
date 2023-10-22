a66e1d1c7dcf8f1bd2596b9302bf3e39fe30052b
stirelshka8 <stirelshka8@gmail.com>
2023-10-22 23:12:49 +0300

    fix

--- add_user.py
+++ add_user.py
@@ -1,79 +1,84 @@
+from getpass import getpass
+import pass_hash
 import subprocess
 import yaml
-from getpass import getpass
 
-# Чтение данных о серверах из файла YAML
-with open('servers.yml', 'r') as server_file:
-    server_data = yaml.safe_load(server_file)
+try:
+    # Чтение данных о серверах из файла YAML
+    with open('servers.yml', 'r') as server_file:
+        server_data = yaml.safe_load(server_file)
 
-# Генерация временного инвентарного файла
-temp_inventory_file = 'temp_inventory.ini'
-with open(temp_inventory_file, 'w') as temp_inventory:
-    temp_inventory.write('[servers]\n')
-    for server_info in server_data:
-        hostname = server_info['hostname']
-        ssh_port = server_info.get('port', 22)  # По умолчанию используется порт 22, если не указан другой
-        temp_inventory.write(f'{hostname} ansible_ssh_port={ssh_port} ansible_ssh_host={hostname}\n')
+    # Генерация временного инвентарного файла
+    temp_inventory_file = 'temp_inventory.ini'
+    with open(temp_inventory_file, 'w') as temp_inventory:
+        temp_inventory.write('[servers]\n')
+        for server_info in server_data:
+            hostname = server_info['hostname']
+            ssh_port = server_info.get('port', 22)  # По умолчанию используется порт 22, если не указан другой
+            temp_inventory.write(f'{hostname} ansible_ssh_port={ssh_port} ansible_ssh_host={hostname}\n')
 
-# Выбор авторизации: пароль или ключ SSH
-authentication_choice = input("Выберите метод авторизации (pass/key): ").strip().lower()
+    # Выбор авторизации
+    authentication_choice = input("Выберите метод авторизации (pass/key): ").strip().lower()
 
-if authentication_choice not in ['pass', 'key']:
-    print("Неправильный выбор авторизации. Введите 'pass' или 'key'.")
-    exit(1)
+    if authentication_choice not in ['pass', 'key']:
+        print("Неправильный выбор авторизации. Введите 'pass' или 'key'.")
+        exit(1)
 
-if authentication_choice == 'pass':
-    password = getpass("Введите пароль для SSH-авторизации: ")
+    if authentication_choice == 'pass':
+        password = getpass("Введите пароль для SSH-авторизации: ")
 
-# Чтение данных о пользователях из файла
-with open('users.yml', 'r') as users_file:
-    users_data = yaml.safe_load(users_file)
+    # Чтение данных о пользователях из файла
+    with open('users.yml', 'r') as users_file:
+        users_data = yaml.safe_load(users_file)
 
-# Генерация Ansible-плейбука
-ansible_playbook = {
-    'name': 'Add users and SSH keys',
-    'hosts': 'servers',
-    'become': 'yes',
-    'gather_facts': 'no',
-    'vars': {
-        'users_data': users_data['users'],
-        'ansible_ssh_pass': password if authentication_choice == 'pass' else None,
-    },
-    'tasks': [
-        {
-            'name': 'Create users',
-            'user': {
-                'name': '{{ item.username }}',
-                'createhome': 'yes',
-                'shell': '/bin/bash',
-            },
-            'loop': '{{ users_data }}',
+    # Генерация плейбука
+    ansible_playbook = {
+        'name': 'Add users and SSH keys',
+        'hosts': 'servers',
+        'become': 'yes',
+        'gather_facts': 'no',
+        'vars': {
+            'users_data': users_data['users'],
+            'ansible_ssh_pass': password if authentication_choice == 'pass' else None,
         },
-        {
-            'name': 'Add SSH keys',
-            'authorized_key': {
-                'user': '{{ item.username }}',
-                'key': '{{ item.ssh_key }}',
+        'tasks': [
+            {
+                'name': 'Create users',
+                'user': {
+                    'name': '{{ item.username }}',
+                    'createhome': 'yes',
+                    'shell': '/bin/bash',
+                    'password': '{{ item.hash_pass  }}',
+                },
+                'loop': '{{ users_data }}',
             },
-            'loop': '{{ users_data }}',
-        },
-        {
-            'name': 'Add users to sudo group',
-            'user': {
-                'name': '{{ item.username }}',
-                'groups': 'sudo',
+            {
+                'name': 'Add SSH keys',
+                'authorized_key': {
+                    'user': '{{ item.username }}',
+                    'key': '{{ item.ssh_key }}',
+                },
+                'loop': '{{ users_data }}',
             },
-            'loop': '{{ users_data }}',
-        },
-    ],
-}
+            {
+                'name': 'Add users to sudo group',
+                'user': {
+                    'name': '{{ item.username }}',
+                    'groups': 'sudo',
+                },
+                'loop': '{{ users_data }}',
+            },
+        ],
+    }
 
-# Сохранение плейбука в файл
-with open('add_users.yml', 'w') as playbook_file:
-    yaml.dump([ansible_playbook], playbook_file, default_flow_style=False)
+    # Сохранение плейбука в файл
+    with open('add_users.yml', 'w') as playbook_file:
+        yaml.dump([ansible_playbook], playbook_file, default_flow_style=False)
 
-# Выполнение плейбука с использованием временного инвентарного файла
-subprocess.call(['ansible-playbook', '-i', temp_inventory_file, 'add_users.yml'])
-
-# Удаление временных файлов
-subprocess.call(['rm', temp_inventory_file, 'add_users.yml'])
+    # Выполнение плейбука с использованием временного инвентарного файла
+    subprocess.call(['ansible-playbook', '-i', temp_inventory_file, 'add_users.yml'])
+except:
+    print("\n\n\nЧто то пошло не так, что конкретно хз...\n\n\n")
+finally:
+    # Удаление временных файлов
+    subprocess.call(['rm', temp_inventory_file, 'add_users.yml'])
