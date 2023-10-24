be0bc3019b4f9d1f4aeddd5e1cd41ebb530ba3d1
stirelshka8 <stirelshka8@gmail.com>
2023-10-24 13:06:45 +0300

    update

--- add_user.py
+++ add_user.py
@@ -1,26 +1,52 @@
+from pass_hash import update_users_passwords
+from write_ip import update_servers_config
 from getpass import getpass
-import pass_hash
+import configparser
 import subprocess
 import yaml
 
+# Создание объекта ConfigParser
+config = configparser.ConfigParser()
+
+# Чтение ini-файла
+config.read('config/config.ini')
+
+temp_inventory_file = config.get('FILES', 'temp_inventory_file')
+file_servers = config.get('FILES', 'file_servers')
+file_settnings = config.get('FILES', 'file_settnings')
+file_user = config.get('FILES', 'file_user')
+file_adduser = config.get('FILES', 'file_adduser')
+file_ip = config.get('FILES', 'file_ip')
+
+update_users_passwords(file_user)
+
 try:
     # Чтение данных о серверах из файла YAML
-    with open('servers.yml', 'r') as server_file:
+    with open(file_servers, 'r') as server_file:
         server_data = yaml.safe_load(server_file)
 
     # Чтение настроек из файла YAML
-    with open('settings.yml', 'r') as settings_file:
+    with open(file_settnings, 'r') as settings_file:
         settings_data = yaml.safe_load(settings_file)
         ssh_user = settings_data['ssh_user']
 
     # Генерация временного инвентарного файла
-    temp_inventory_file = 'temp_inventory.ini'
+  
     with open(temp_inventory_file, 'w') as temp_inventory:
         temp_inventory.write('[servers]\n')
         for server_info in server_data:
             hostname = server_info['hostname']
             ssh_port = server_info.get('port', 22)  # По умолчанию используется порт 22, если не указан другой
             temp_inventory.write(f'{hostname} ansible_ssh_port={ssh_port} ansible_ssh_host={hostname}\n')
+
+    update_choice = input("Обновить файл серверов (yes/no): ").strip().lower()
+
+    if update_choice not in ['yes', 'no']:
+        print("Неправильный выбор авторизации. Введите 'pass' или 'key'.")
+        exit(1)
+
+    if update_choice == 'yes':
+        update_servers_config(file_ip, file_servers)
 
     # Выбор авторизации
     authentication_choice = input("Выберите метод авторизации (pass/key): ").strip().lower()
@@ -61,7 +87,7 @@ try:
             }
 
     # Чтение данных о пользователях из файла
-    with open('users.yml', 'r') as users_file:
+    with open(file_user, 'r') as users_file:
         users_data = yaml.safe_load(users_file)
 
     # Генерация плейбука
@@ -111,13 +137,13 @@ try:
     ])
 
     # Сохранение плейбука в файл
-    with open('add_users.yml', 'w') as playbook_file:
+    with open(file_adduser, 'w') as playbook_file:
         yaml.dump([ansible_playbook], playbook_file, default_flow_style=False)
 
     # Выполнение плейбука с использованием временного инвентарного файла
-    subprocess.call(['ansible-playbook', '-i', temp_inventory_file, 'add_users.yml', '-e', 'ansible_remote_tmp=/tmp'])
+    subprocess.call(['ansible-playbook', '-i', temp_inventory_file, file_adduser, '-e', 'ansible_remote_tmp=/tmp'])
 except Exception as e:
     print(f"Произошла ошибка: {str(e)}")
 finally:
     # Удаление временных файлов
-    subprocess.call(['rm', temp_inventory_file, 'add_users.yml'])
+    subprocess.call(['rm', temp_inventory_file, file_adduser])
--- pass_hash.py
+++ pass_hash.py
@@ -1,19 +1,19 @@
 import yaml
 from passlib.hash import sha512_crypt
 
-# Функция для шифрования пароля
 def encrypt_password(password):
     return sha512_crypt.using(rounds=5000).hash(password)
 
-# Прочитать существующие данные из users.yml
-with open('users.yml', 'r') as users_file:
-    users_data = yaml.safe_load(users_file)
+def update_users_passwords(users_file_path):
+    # Прочитать существующие данные из файла
+    with open(users_file_path, 'r') as users_file:
+        users_data = yaml.safe_load(users_file)
 
-# Шифрование и сохранение
-for user in users_data['users']:
-    if 'password' in user:
-        user['hash_pass'] = encrypt_password(user['password'])
+    # Шифрование и сохранение
+    for user in users_data['users']:
+        if 'password' in user:
+            user['hash_pass'] = encrypt_password(user['password'])
 
-# Запись обновленных данных обратно в файл users.yml
-with open('users.yml', 'w') as users_file:
-    yaml.dump(users_data, users_file, default_flow_style=False)
+    # Запись обновленных данных обратно в файл
+    with open(users_file_path, 'w') as users_file:
+        yaml.dump(users_data, users_file, default_flow_style=False)
