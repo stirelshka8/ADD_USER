cbb89d5b9b8146b0da98719f015eb162264a051a
stirelshka8 <stirelshka8@gmail.com>
2023-10-25 19:56:59 +0300

    Добавлена функция автоматического отпечатка

--- add_user.py
+++ add_user.py
@@ -1,9 +1,11 @@
+import time
 from pass_hash import update_users_passwords
 from write_ip import update_servers_config
 from getpass import getpass
 import configparser
 import subprocess
 import yaml
+import os
 
 # Создание объекта ConfigParser
 config = configparser.ConfigParser()
@@ -19,6 +21,41 @@ file_adduser = config.get('FILES', 'file_adduser')
 file_ip = config.get('FILES', 'file_ip')
 
 update_users_passwords(file_user)
+
+
+def get_known_hosts_path():
+    home_directory = os.path.expanduser("~")
+    
+    known_hosts_path = os.path.join(home_directory, ".ssh", "known_hosts")
+    
+    return known_hosts_path
+
+def is_host_in_known_hosts(known_hosts_file, hostname):
+    with open(known_hosts_file, "r") as file:
+        for line in file:
+            if hostname in line:
+                return True
+    return False
+
+def update_known_hosts(hostname, port):
+    known_hosts_file_path = get_known_hosts_path()
+    
+    try:
+        os.makedirs(os.path.dirname(known_hosts_file_path), exist_ok=True)
+        
+        known_hosts_entry = subprocess.check_output(["sudo", "ssh-keyscan", "-p", str(port), hostname], text=True)
+
+        if not is_host_in_known_hosts(known_hosts_file_path, hostname):
+            with open(known_hosts_file_path, "a") as known_hosts_file:
+                known_hosts_file.write(known_hosts_entry)
+        else:
+            print(f"Host '{hostname}' is already in known_hosts. Skipping update.")
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error running ssh-keyscan: {str(e)}")
+    except Exception as e:
+        print(f"Error adding host key to known_hosts: {str(e)}")
+
 
 try:
     # Чтение данных о серверах из файла YAML
@@ -47,6 +84,8 @@ try:
 
     if update_choice == 'yes':
         update_servers_config(file_ip, file_servers)
+        time.sleep(1)
+        update_known_hosts(hostname, ssh_port)
 
     # Выбор авторизации
     authentication_choice = input("Выберите метод авторизации (pass/key): ").strip().lower()
@@ -71,7 +110,8 @@ try:
         'vars': {
             'users_data': users_data['users'],
             'ansible_ssh_pass': password if authentication_choice == 'pass' else None,
-            'ansible_ssh_user': ssh_user},
+            'ansible_ssh_user': ssh_user
+        },
         'tasks': []
     }
 
@@ -96,10 +136,9 @@ try:
         },
         {
             'name': 'Add users to sudo group',
-            'user': {
-                'name': '{{ item.username }}',
-                'groups': 'sudo',
-            },
+            'become': 'yes',
+            'become_user': 'root',
+            'command': 'usermod -aG sudo {{ item.username }}',
             'loop': '{{ users_data }}',
         },
     ])
